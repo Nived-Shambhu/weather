6a4abf444ca8698da66c4ebf9be6ff93eb9d0948
Nived-Shambhu <nivedpanil2004@gmail.com>
2026-06-01 11:29:50 +0530

    Add DynamoDB stream event handling

---
+++ Lambda/dynamodb_to_s3_lambda.py
@@ -0,0 +1,13 @@
+import json
+import boto3
+
+s3 = boto3.client('s3')
+
+BUCKET_NAME = 'weather-project-bucket-123'
+
+def lambda_handler(event, context):
+    pass
+
+for record in event['Records']:
+    if record['eventName'] in ['INSERT', 'MODIFY']:
+        pass
